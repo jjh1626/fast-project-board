8b26569f00506854f68f031b7e924a02258cf119
joko <jjh1626@gmail.com>
2024-01-12 01:22:35 +0900

    #10 - repository interface 작성
    이걸로 db 에 접

---
+++ src/main/java/com/fast/projectboard/repository/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.fast.projectboard.repository;
+
+import com.fast.projectboard.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
