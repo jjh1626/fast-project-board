8791899068a7016737c7e7d9490290c261593f8c
joko <jjh1626@gmail.com>
2024-01-14 13:25:26 +0900

    #5 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- src/main/java/com/fast/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fast/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,30 @@
 package com.fast.projectboard.repository;
 
 import com.fast.projectboard.domain.Article;
+import com.fast.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);   //제한적으로 지정한 검색만 가능 하도록
+        //검색될 항목 지정
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);  //쿼리형태: like '%%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
